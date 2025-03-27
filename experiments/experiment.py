ef63005f004a0849dd2b8601bc5d4701d000944d
Andreas Hofacker <kai_andreas.hofacker2@tu-dresden.de>
2025-03-27 16:38:14 +0100

    Add hysteresis by time-delay of applying gate

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -13,7 +13,7 @@ def rhs(y, t, k, tau):
 
     # oect 1 gate
     dydt[2] = (y[0] - y[2]) / tau
-    out1 = sigmoid(y[2], 0.1, 0)
+    out1 = sigmoid(y[2], 0.05, 0)
 
     # oect 2 gate
     dydt[3] = (out1 - y[3]) / tau
@@ -25,8 +25,8 @@ def sigmoid(x, s, offset=0):
     return 1.0 / (1.0 + np.exp(-(x - offset) / s))
 
 
-k = 4
-tau = 2
+k = 1
+tau = 0.1
 
 tmax = 20
 
@@ -34,10 +34,13 @@ y0 = np.array([1, 0, 0, 0])
 ts = np.arange(0, tmax, 0.05)
 
 y_sol = sci.odeint(rhs, y0, ts, args=(k, tau))
-out2 = sigmoid(y_sol[:, 3], 0.1, 0)
-y = np.hstack((y_sol, out2.reshape((-1, 1))))
+
+out1 = sigmoid(y_sol[:, 2], 0.05, 0)
+out2 = sigmoid(y_sol[:, 3], 0.05, 0)
+y = np.hstack((y_sol, out1.reshape(-1, 1), out2.reshape(-1, 1)))
 
 _ = phase_portrait(y, (0, 4))
-_ = time_series(ts, y, (0, 2, 3, 4))
+_ = phase_portrait(y, (4, 5))
+_ = time_series(ts, y)
 
 plt.show()
